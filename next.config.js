007b8263d829a9714085d204265a324ca9722636
Radix-Obsidian <Hello@abodeai.co>
2025-09-02 00:26:24 -0400

    Resolve merge conflicts in discover page and update next config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,40 @@ const nextConfig = {
     SPOONACULAR_BASE_URL: process.env.SPOONACULAR_BASE_URL || 'https://api.spoonacular.com/recipes',
     SPOONACULAR_RATE_LIMIT: process.env.SPOONACULAR_RATE_LIMIT || '5000',
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'edamam-product-images.s3.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'spoonacular.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.spoonacular.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'fdc.nal.usda.gov',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
   experimental: {
     serverComponentsExternalPackages: ['@supabase/supabase-js'],
   },
